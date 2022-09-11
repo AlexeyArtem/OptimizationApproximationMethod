70f6e3be9cffae3e5fea412e58e4f2461ea05db1
Ivan <ivan.zhivodovsky32@mail.ru>
2022-09-11 15:08:29 +0300

    null

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace CSharp
 {
@@ -10,6 +12,17 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            List item = await JsonFileReader.ReadAsync<Item>(@"C:\myFile.json");
+
+        }
+    }
+
+    public static class JsonFileReader
+    {
+        public static async Task<T> ReadAsync<T>(string filePath)
+        {
+            FileStream stream = File.OpenRead(filePath);
+            return await JsonSerializer.DeserializeAsync<T>(stream);
         }
     }
 }
